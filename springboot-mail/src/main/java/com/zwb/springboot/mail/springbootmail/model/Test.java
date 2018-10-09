b8064859b19c4bb98d1218d2ca58583e25a8f77c
文博 <40294244+zhouwenbo81@users.noreply.github.com>
2018-10-09 15:40:13 +0800

    新增: Springboot整合邮箱新增测试类

---
+++ springboot-mail/src/main/java/com/zwb/springboot/mail/springbootmail/model/Test.java
@@ -0,0 +1,19 @@
+package com.zwb.springboot.mail.springbootmail.model;
+
+/**
+ * <p>
+ * Title: Test
+ * </p>
+ * <p>
+ * Description: 测试类
+ * </p>
+ * <p>
+ * Company: 都市放牛集团
+ * </p>
+ *
+ * @author zhouwenbo
+ * @version 1.0
+ * @date 2018/10/9 15:38
+ */
+public class Test {
+}
